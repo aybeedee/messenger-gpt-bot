912c75eeb50244f4441c0b380743087847506eaf
AbdullahUmer575 <abdullahumer575@gmail.com>
2023-04-24 23:18:17 +0500

    added docs format post req, still getting error #100 though

--- fbAPI.py
+++ fbAPI.py
@@ -6,13 +6,26 @@ import requests
 
 load_dotenv()
 
+USER_ID = os.getenv("USER_ID")
+PAGE_ID = os.getenv("PAGE_ID")
 LATEST_API_VERSION = "v16.0"
-PAGE_ACCESS_TOKEN = os.getenv('PAGE_ACCESS_TOKEN')
-CONVERSATION_ID = os.getenv('CONVERSATION_ID')
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
+CONVERSATION_ID = os.getenv("CONVERSATION_ID")
 
+# arguments = {"fields": "messages{message}", "access_token": PAGE_ACCESS_TOKEN}
+# r = requests.get(f"https://graph.facebook.com/{LATEST_API_VERSION}/{CONVERSATION_ID}",
+#                  params=arguments)
 
-arguments = {"fields": "messages{message}", "access_token": PAGE_ACCESS_TOKEN}
-r = requests.get(f"https://graph.facebook.com/{LATEST_API_VERSION}/{CONVERSATION_ID}",
+# print(r.json())
+
+arguments = {
+    "recipient": {"id": USER_ID}, 
+    "messaging_type": "RESPONSE", 
+    "message": {"text": "You finally did it"}, 
+    "access_token": PAGE_ACCESS_TOKEN
+    }
+
+r = requests.post(f"https://graph.facebook.com/{LATEST_API_VERSION}/{PAGE_ID}/messages",
                  params=arguments)
 
 print(r.json())
