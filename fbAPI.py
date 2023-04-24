79901d383f6a13d176cd1841c97032f695145c9d
AbdullahUmer575 <abdullahumer575@gmail.com>
2023-04-24 23:10:58 +0500

    removed sensitive cache

--- fbAPI.py
+++ fbAPI.py
@@ -4,12 +4,6 @@ import requests
 
 # requests.get(URL, parameter={key: value}, arguments)
 
-
-# https://graph.facebook.com/v16.0/t_6194435950612806
-# ?
-# fields=messages{message}
-# &access_token=EAAJ2zxrtrIMBAKWZCPZB20GlbxrJviCLpzQaS61q4sxOrIReKMyMHDWmKn9VM6isbemDmHGUEec9NQEZC3N8s5ZABZBMuxfHX70rBAGFbZBjGyMfFEuUPiX5TM0DwVWnFE1mwzIVMnB3zQ2MqucStUhdoVCZCijCyFyaWVkJz2beS3LkgNncaSX4ORKKl45hTh4rF802vtD6H3aE2OBxLub
-
 load_dotenv()
 
 LATEST_API_VERSION = "v16.0"
